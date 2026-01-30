2d7baa3a1d648456cb23e4acb64e0f6223c9e04a
e-afzal <essam.afzal@outlook.com>
2026-01-30 08:36:50 +0400

    Header created

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+// LOCAL COMPONENT
+import {Button} from "@/components/ui/button";
+
+const Home = () => {
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            Home
+        </div>
+    );
+};
+export default Home;
